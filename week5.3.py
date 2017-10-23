32998659eaba1794b4cce48bf1ee29bdfd59e4f4
21350010 <32065584+21350010@users.noreply.github.com>
2017-10-23 14:23:51 +0100

    week5.3
    mistakes rectified, numbers must be integers

---
+++ week5.3.py
@@ -0,0 +1,12 @@
+# number1=input("Enter first number")
+# number2=input("Enter second number")
+# result=number1+number2
+# print("The result is"+result)
+# Rectified 
+
+number1=int(input("Enter first number:"))
+number2=int(input("Enter second number:"))
+
+result=number1+number2
+
+print("The result is:\t",+result)
